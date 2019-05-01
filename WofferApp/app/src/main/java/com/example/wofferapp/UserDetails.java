7ba495437e115d2adbcf2a1ac5159248b5a99d85
mahew <mahew@DESKTOP-9S1CATF>
2019-05-01 16:27:14 +0100

    More changes

--- WofferApp/app/src/main/java/com/example/wofferapp/UserDetails.java
+++ WofferApp/app/src/main/java/com/example/wofferapp/UserDetails.java
@@ -1,4 +1,12 @@
 package com.example.wofferapp;
 
+import java.lang.reflect.Array;
+
 public class UserDetails {
+
+    private String id;
+    private int currentOfferid;
+    private Array completedOffers;
+    private int colorTheme;
+
 }
