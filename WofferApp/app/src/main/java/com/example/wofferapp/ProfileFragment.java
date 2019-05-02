052e776c3bb18e78766c8eb5e00f7f31ba5897ef
mahew <mahew@DESKTOP-9S1CATF>
2019-05-02 15:10:06 +0100

    Update profile

--- WofferApp/app/src/main/java/com/example/wofferapp/ProfileFragment.java
+++ WofferApp/app/src/main/java/com/example/wofferapp/ProfileFragment.java
@@ -1,19 +1,90 @@
 package com.example.wofferapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class ProfileFragment extends Fragment {
+
+    public UserDetails currentUser = new UserDetails();
+
+    public void setCurrentUser(UserDetails us){
+        currentUser = us;
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        int SDK_INT = android.os.Build.VERSION.SDK_INT;
+        if (SDK_INT > 8)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
+                    .permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        TextView offerTitle = ((TextView) v
+        .findViewById(R.id.profileText));
+
+        offerTitle.setText(currentUser.getId());
+
+        offerTitle.setText(getFirebaseUser().getDisplayName());
+
+        ImageView offerImg = ((ImageView) v
+                .findViewById(R.id.profileImage));
+
+        offerImg.setImageBitmap(getImageBitmap(getFirebaseUser().getPhotoUrl().toString()));
+
+        return v;
+
+    }
+
+    public FirebaseUser getFirebaseUser() {
+        return FirebaseAuth.getInstance().getCurrentUser();
+    }
+
+    private Bitmap getImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            //Log.e(TAG, "Error getting bitmap", e);
+        }
+        return bm;
     }
 
 }
