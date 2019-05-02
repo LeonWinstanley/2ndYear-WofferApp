bc8c0fbf10eeb7486bf814b84a08394d45271f4f
mahew <mahew@DESKTOP-9S1CATF>
2019-05-02 18:32:32 +0100

    Add location listener functionality

--- WofferApp/app/src/main/java/com/example/wofferapp/OffersFragment.java
+++ WofferApp/app/src/main/java/com/example/wofferapp/OffersFragment.java
@@ -1,30 +1,24 @@
 package com.example.wofferapp;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Build;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.location.Location;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -40,26 +34,31 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
 import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
 
-public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowClickListener, OnMapReadyCallback {
+public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowClickListener
+        , OnMapReadyCallback {
 
     // Create a map object
     GoogleMap mMap;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    public UserDetails currentUser = new UserDetails();
+    OfferDetails currentOffer;
+    LocationRequest mLocationRequest;
+    Location mLastLocation;
+    FusedLocationProviderClient mFusedLocationClient;
+
     public OffersFragment() {
         // Empty Constructor
     }
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    public UserDetails currentUser = new UserDetails();
 
     public void setCurrentUser(UserDetails us){
         currentUser = us;
@@ -72,11 +71,21 @@ public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowCl
     // When the view is first started, inflate it to the container size
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
         View v = inflater.inflate(R.layout.fragment_offers, container, false);
         return v;
+    }
 
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        //stop location updates when Activity is no longer active
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
     }
 
     // Once onCreateView has returned a view, get the map object asynchronously once the
@@ -90,7 +99,8 @@ public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowCl
                     .permitAll().build();
             StrictMode.setThreadPolicy(policy);
         }
-        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager()
+                .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
@@ -99,14 +109,23 @@ public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowCl
     // enabled. Possibly ask for them again? Ignore for now
     @Override
     public void onMapReady(GoogleMap googleMap) {
-
         mMap = googleMap;
         mMap.setOnInfoWindowClickListener(this);
         mMap.setInfoWindowAdapter(setMarkerWindow());
-
         LatLng trent = new LatLng(52.9117779,-1.1854268);
         LatLng nottingham = new LatLng(52.9493, -1.1471);
-        //mMap.addMarker(new MarkerOptions().position(trent).title("Marker in Nottingham"));
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(100); // two minute interval
+        mLocationRequest.setFastestInterval(100);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        try{mFusedLocationClient.requestLocationUpdates(mLocationRequest,
+                mLocationCallback,
+                Looper.myLooper());}
+        catch (SecurityException e){
+            // No Location Perms
+        }
 
         db.collection("offers")
                 //.whereEqualTo("capital", true)
@@ -116,16 +135,12 @@ public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowCl
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-
                                 OfferDetails offer = document.toObject(OfferDetails.class);
-
                                 double latitude = offer.getPosition().getLatitude();
                                 double longitude = offer.getPosition().getLongitude();
                                 LatLng location = new LatLng(latitude, longitude);
-
                                 Marker newMark = mMap.addMarker(new MarkerOptions()
                                         .position(location));
-
                                 newMark.setTag(offer);
                             }
                         } else {
@@ -133,37 +148,71 @@ public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowCl
                         }
                     }
                 });
-
         try {
             mMap.setMyLocationEnabled(true);
         } catch (SecurityException e) {
             // No location services, maybe a dialog here?
         }
-
         // Finally, move the camera to the marker and set the zoom to 15
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(trent, 15.0f));
-
     }
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            List<Location> locationList = locationResult.getLocations();
+            if (locationList.size() > 0) {
+                //The last location in the list is the newest
+                Location location = locationList.get(locationList.size() - 1);
+                mLastLocation = location;
+                db.collection("offers")
+                        .whereEqualTo("id", currentUser.getCurrentOfferid())
+                        .get()
+                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                if (task.isSuccessful()) {
+                                    for (QueryDocumentSnapshot document : task.getResult()) {
+                                        currentOffer = document.toObject(OfferDetails.class);
+                                        if(location.getLatitude() < currentOffer.getPosition().getLatitude()+0.0001f &&
+                                                location.getLatitude() > currentOffer.getPosition().getLatitude()-0.0001f &&
+                                                location.getLongitude() < currentOffer.getPosition().getLongitude()+0.0001f &&
+                                                location.getLatitude() > currentOffer.getPosition().getLatitude()-0.0001f){
+                                            Toast.makeText(getContext(), "OFFER GOTTT", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                                } else {
+                                    //Log.d(TAG, "Error getting documents: ", task.getException());
+                                }
+                            }
+                        });
+
+
+            }
+        }
+    };
 
     @Override
     public void onInfoWindowClick(Marker marker) {
         OfferDetails markerTag = (OfferDetails) marker.getTag();
         currentUser.setCurrentOfferid(markerTag.getID());
         ((MainActivity)getActivity()).currUser = currentUser;
-
-        DocumentReference usersRef = db.collection("users").document(getFirebaseUser().getUid());
+        DocumentReference usersRef = db.collection("users")
+                .document(getFirebaseUser().getUid());
         usersRef
                 .update("currentOfferid", markerTag.getID())
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
                     public void onSuccess(Void aVoid) {
-                        Toast.makeText(getContext(), "Offer Added!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Offer Added!", Toast.LENGTH_SHORT)
+                                .show();
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(getContext(), "Error Adding Offer", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(getContext(), "Error Adding Offer", Toast.LENGTH_SHORT)
+                                .show();
                     }
                 });
     }
@@ -174,24 +223,16 @@ public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowCl
             public View getInfoWindow(Marker marker) {
                 View myContentView = getLayoutInflater().inflate(
                         R.layout.info_marker, null);
-
                 OfferDetails markerTag = (OfferDetails) marker.getTag();
-
                 //TextView offerTitle = ((TextView) myContentView
                         //.findViewById(R.id.title));
-
                 //offerTitle.setText(markerTag.getTitle());
-
                 //TextView offerDesc = ((TextView) myContentView
                        //.findViewById(R.id.description));
-
                 //offerDesc.setText(markerTag.getDescription());
-
                 ImageView offerImg = ((ImageView) myContentView
                         .findViewById(R.id.image));
-
                 offerImg.setImageBitmap(getImageBitmap(markerTag.getImg()));
-
                 return myContentView;
             }
 
@@ -218,5 +259,4 @@ public class OffersFragment extends Fragment implements GoogleMap.OnInfoWindowCl
             }
         };
     }
-
 }
