a5ecbac9b23a8a124c409100d1f2aaff3a17a61a
Leon Winstanley <leon.winstanley2017@my.ntu.ac.uk>
2019-05-04 01:50:16 +0100

    Changed the design a bit. Looks a bit better

--- WofferApp/app/src/main/java/com/example/wofferapp/MainActivity.java
+++ WofferApp/app/src/main/java/com/example/wofferapp/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.annotation.NonNull;
 import android.util.Log;
 import android.view.MenuItem;
 import android.Manifest;
+import android.app.Activity;
+import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
