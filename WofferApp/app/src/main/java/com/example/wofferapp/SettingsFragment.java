ba567422f121a4141a7a9c6f9cfc5ceeefec9f76
Leon Winstanley <leon.winstanley2017@my.ntu.ac.uk>
2019-05-05 14:28:55 +0100

    User details are now shown on settings. removed theme sel

--- WofferApp/app/src/main/java/com/example/wofferapp/SettingsFragment.java
+++ WofferApp/app/src/main/java/com/example/wofferapp/SettingsFragment.java
@@ -1,7 +1,11 @@
 package com.example.wofferapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -10,28 +14,86 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.BufferedInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class SettingsFragment extends Fragment {
 
-    public FirebaseUser getFirebaseUser() {
-        return FirebaseAuth.getInstance().getCurrentUser();
-    }
-
-    ;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View vi = inflater.inflate(R.layout.fragment_settings, container, false);
+
+
+
+        TextView txt = ((TextView) vi.findViewById(R.id.ProfileName));
+
+        ImageView img = ((ImageView) vi.findViewById(R.id.ProfileImage));
+
+
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+
+
+
+
+            Uri photoUri = user.getPhotoUrl();
+            String photoURL = photoUri.toString();
+
+            Bitmap image = getImageBitmap(photoURL);
+
+            txt.setText(name);
+            img.setImageBitmap(image);
+
+        }
+
+
+
+
+
+
+
+
+
+
         return vi;
 
     }
-
-
+    private Bitmap getImageBitmap(String url) {
+        Bitmap bm = null;
+        try {
+            URL aURL = new URL(url);
+            URLConnection conn = aURL.openConnection();
+            conn.connect();
+            InputStream is = conn.getInputStream();
+            BufferedInputStream bis = new BufferedInputStream(is);
+            bm = BitmapFactory.decodeStream(bis);
+            bis.close();
+            is.close();
+        } catch (IOException e) {
+            //Log.e(TAG, "Error getting bitmap", e);
+        }
+        return bm;
+    }
 
 }
 
